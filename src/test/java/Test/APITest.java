8077424777f4041d4cab24127c5e6561431ff680
tarakandi <omarfaroque@MacBook-Pro.local>
2022-03-17 15:26:25 -0400

    New Function

--- src/test/java/Test/APITest.java
+++ src/test/java/Test/APITest.java
@@ -8,6 +8,10 @@ public class APITest {
   {
 	  System.out.println("This is not an API Test");
 	  System.out.println("Test out pull request from cmd");
-	  System.out.println("Testing out branch");
+  }
+  
+  public void secondAPI()
+  {
+	  System.out.println("myBranch");
   }
 }
